21a1f88261c66bc641c4a6d4c64a5100d43555b9
muhreynaldii <reynaldi180101@gmail.com>
2022-09-14 14:49:56 +0700

    first commit

---
+++ src/App.vue
@@ -0,0 +1,11 @@
+<script>
+// This starter template is using Vue 3 <script setup> SFCs
+// Check out https://vuejs.org/api/sfc-script-setup.html#script-setup
+import HelloWorld from "./components/HelloWorld.vue";
+</script>
+
+<template>
+  <p class="text-green-500 text-lg font-bold">INI TAILWIND</p>
+</template>
+
+<style></style>
